f52504e6d2dd54b24d4fa9636d4914f963969791
rere <regis-graptin@sfr.fr>
2024-12-17 12:11:44 +0700

    feat: add tailwindcss material

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Example from '../components/Example';
 
 const Home: NextPage = () => {
   return (
@@ -17,6 +18,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <ConnectButton />
+        <Example />
       </main>
 
       <footer className={styles.footer}>
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -7,5 +9,5 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+})
 
